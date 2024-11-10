a54cf94de2ed8442e33e662897036369e307b0f9
Facundo Gonzalez <fp.gonzalez@alumno.etec.um.edu.ar>
2024-11-10 17:58:39 -0300

    commit

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -3,6 +3,9 @@ import { ACCESS_TOKEN } from "../constants";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
